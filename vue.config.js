7b5e4992447ece408dafc6136ba0404b983fb835
myojoojang <mjjang0127@gmail.com>
2021-04-03 17:17:33 +0900

    'test'

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  devServer:{
-    proxy:`https://collectionapi.metmuseum.org/public/collection/v1`
-  }
+  // devServer:{
+  //   proxy:`https://collectionapi.metmuseum.org/public/collection/v1`
+  // }
 }
